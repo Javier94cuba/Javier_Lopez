c355fd75b792488607a018fdcfea86a9ef9c81c5
Javier Lopéz <jlopezhdez94@gmail.com>
2023-10-15 13:09:13 +0100

    update

--- Javier_Lopez_CV/src/components/containers/List_Projects.tsx
+++ Javier_Lopez_CV/src/components/containers/List_Projects.tsx
@@ -1,8 +1,6 @@
 import Project from "../atoms/Project"
 import cubaphotos from "/src/assets/images/cubanphotos.png";
 import wordpress from "/src/assets/images/wordpress.png"
-import cv from "/src/assets/images/cv.png"
-import react_js from "/src/assets/images/react.png"
 function Portfolio() {
   return (
     <>
@@ -10,7 +8,6 @@ function Portfolio() {
    <h1 id="proyects" className="text-4xl font-bold text-center text-slate-950 dark:text-slate-200">Portafolio de proyectos</h1>
    <div className="grid-cols-3 items-center justify-center ml-40">
     <Project text_img="Wordpress" img_tec={wordpress} image={cubaphotos} name="Cubaphotostrips"/>
-    <Project text_img="React JS" img_tec={react_js} image={cv} name="Curriculum Vitae"/>
     </div>
     </div>
     </>
