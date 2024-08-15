5d8eb5658a2146ad88abb08b0ec1099388ee189b
Javier López <jlopezhdez94@gmail.com>
2024-08-15 14:00:01 -0400

    update

--- Javier_Lopez_CV/src/components/containers/About_me.tsx
+++ Javier_Lopez_CV/src/components/containers/About_me.tsx
@@ -1,8 +1,6 @@
  
-
-
+import react_js from "/src/assets/images/wordpresss.png";
 import { CloudArrowUpIcon, LockClosedIcon, ServerIcon, ArrowsPointingOutIcon, ChartPieIcon, ChatBubbleLeftRightIcon, PencilSquareIcon, RectangleGroupIcon, ShieldExclamationIcon, UserCircleIcon } from '@heroicons/react/20/solid'
-import react_js from "/src/assets/images/reacts.png";
 
 function About_me() {
   return (
@@ -49,11 +47,11 @@ function About_me() {
           </div>
         </div>
         <div className="-ml-12 -mt-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden">
-          {/* <img
+          <img
             src={react_js}
-            className="w-[48rem] max-w-none rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem]"
-          /> */}
-          <img className=" h-7" src={react_js}/>
+            // className="w-[48rem] max-w-none rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem]"
+            className="h-72"
+          />
         </div>
         <div className="lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
           <div className="lg:pr-4">
